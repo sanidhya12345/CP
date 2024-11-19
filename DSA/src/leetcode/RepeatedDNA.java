18b639de2c2972e5971bdf886fd6b34b7dd3ce46
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-19 17:42:04 +0530

    updated

--- DSA/src/leetcode/RepeatedDNA.java
+++ DSA/src/leetcode/RepeatedDNA.java
@@ -11,21 +11,20 @@ public class RepeatedDNA {
 	    int n = s.length();
 	    int i = 0;
 	    StringBuilder sb = new StringBuilder();
-	    
-	    while (i + 10 <= n) {  // Ensure there's enough room for a 10-length sequence
-	        sb.setLength(0);  // Reset the StringBuilder for each new sequence
-	        sb.append(s.substring(i, i + 10));  // Get the 10-character substring
+	    while (i + 10 <= n) { 
+	        sb.setLength(0);  
+	        sb.append(s.substring(i, i + 10));  
 	        
 	        if (!map.containsKey(sb.toString())) {
 	            map.put(sb.toString(), 1);
 	        } else {
 	            if (map.get(sb.toString()) == 1) {
-	                ans.add(sb.toString());  // Only add when it repeats for the first time
+	                ans.add(sb.toString());  
 	            }
 	            map.put(sb.toString(), map.get(sb.toString()) + 1);
 	        }
 	        
-	        i++;  // Move the window by 1 character
+	        i++;  
 	    }
 
 	    return ans;
