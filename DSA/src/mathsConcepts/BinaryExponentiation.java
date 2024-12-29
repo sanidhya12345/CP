58ed6b5315e3e5f3dc19992c47af8e8ba02a9070
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-12-29 20:37:37 +0530

    directory update

--- DSA/src/mathsConcepts/BinaryExponentiation.java
+++ DSA/src/mathsConcepts/BinaryExponentiation.java
@@ -16,12 +16,26 @@ public class BinaryExponentiation {
 			return res*res*a;
 		}
 	}
+	//iterative code:
+	public static long binPow(long a,long b) {
+		long res=1;
+		
+		while(b>0) {
+			if((b & 1)!=0) {
+				res=res*a;
+			}
+			a=a*a;
+			b>>=1;
+		}
+		return res;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		long a=sc.nextLong();
 		long b=sc.nextLong();
 		System.out.println(binaryPow(a, b));
+		System.out.println(binPow(a, b));
 		sc.close();
 	}
 
